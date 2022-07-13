252ea5a9878e955809644d72e3e7759d9b957b4c
Tim Shi <tim@cresta.ai>
2022-07-12 17:01:35 -0700

    Add frontend

---
+++ frontend/src/imagen/dalle.js
@@ -0,0 +1,34 @@
+function timeout(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export const generate_photos = async (prompt) => {
+  const num_max_retry = 100;
+
+  // TODO: sanity check prompt.
+  const response_task = await fetch(
+    "http://localhost:8888/dalle/create?" +
+      new URLSearchParams({
+        prompt,
+      })
+  );
+
+  const task = await response_task.json();
+
+  if (task && task.id) {
+    for (let it = 0; it < num_max_retry; it++) {
+      const response_task_updated = await fetch(
+        "http://localhost:8888/dalle/task?" +
+          new URLSearchParams({ id: task.id })
+      );
+      const task_updated = await response_task_updated.json();
+      console.log("dall e", task_updated);
+      if (task_updated && task_updated.status === "succeeded") {
+        return task_updated.generation;
+      }
+      await timeout(1000);
+    }
+  } else {
+    console.error("Failed to create DALL E task", task);
+  }
+};
